d4d751c9f22b570543b8b95b5e4b6a65d107debb
SomebodyFromSomewhere <Andreev2168@gmail.com>
2022-02-12 13:54:45 +0300

    Added i/o

---
+++ L1T1/L1T1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace L1T1
+{
+    class Greet
+    {
+        public static int Main()
+        {
+            Console.WriteLine("Введите имя: ");
+            string input = Console.ReadLine(); //Можно ли как нибуть убрать ошибку CS8600 и её следствие CS8604, кромя способа с warning disable?
+
+            Console.WriteLine("Здравствуйте " + input + ".");
+
+            return 0;
+        }
+    }
+}
