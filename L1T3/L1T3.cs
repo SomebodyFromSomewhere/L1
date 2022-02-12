182d655720ae4ac59c465cc99ab8b07725803c77
SomebodyFromSomewhere <Andreev2168@gmail.com>
2022-02-12 14:13:03 +0300

    L1T3: Добавлен вывод результатов

--- L1T3/L1T3.cs
+++ L1T3/L1T3.cs
@@ -14,6 +14,7 @@ namespace L1T3
             double alpha = 0;
 
             Console.WriteLine("Введиите альфа: ");
+
             try
             {
                 alpha = Convert.ToDouble(Console.ReadLine());
@@ -23,10 +24,14 @@ namespace L1T3
                 Console.WriteLine(ex.ToString());
             }
             
-
             double z1 = (Math.Cos(alpha) + Math.Sin(alpha)) / (Math.Cos(alpha) - Math.Sin(alpha));
-
             double z2 = (Math.Tan(2 * alpha) + sec(alpha));
+
+            Console.WriteLine("z1 = (cos@ + sin@) / (cos@ - sin@)");
+            Console.WriteLine("z1 = " + z1);
+
+            Console.WriteLine("z2 = tg@ + sec@");
+            Console.WriteLine("z2 = " + z2);
 
             return 0;
         }
