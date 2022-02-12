adfeda53df105a0e1c36762ef98c12dc314f080e
SomebodyFromSomewhere <Andreev2168@gmail.com>
2022-02-12 13:59:57 +0300

    Добавлен вывод информации о ОС

---
+++ L1T2/L1T2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace L1T2
+{
+    public class Info
+    {
+        public static int Main()
+        {
+            var os = Environment.OSVersion;
+            Console.WriteLine("Информация о текущей ОС:\n");
+            Console.WriteLine("Платформа: {0:G}", os.Platform);
+            Console.WriteLine("Версия: {0}", os.VersionString);
+            Console.WriteLine("Информация о версии:");
+            Console.WriteLine("   Основная: {0}", os.Version.Major);
+            Console.WriteLine("   Второстепенная: {0}", os.Version.Minor);
+            Console.WriteLine("Пакет обновления: '{0}'", os.ServicePack);
+
+            return 0;
+        }
+    }
+}
