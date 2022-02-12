5812ba1a0ee77f843be49ebb813ff274a4480651
SomebodyFromSomewhere <Andreev2168@gmail.com>
2022-02-12 13:56:09 +0300

    Вывод вынесен в отдельную функцию

--- L1T1/L1T1.cs
+++ L1T1/L1T1.cs
@@ -4,12 +4,21 @@ namespace L1T1
 {
     class Greet
     {
+        public static void GreetUser(string userName)
+        {
+            if (userName == null || userName == "")
+            {
+                userName = "Unknown";
+            }
+
+            Console.WriteLine("Здравствуйте " + userName + ".");
+        }
         public static int Main()
         {
             Console.WriteLine("Введите имя: ");
             string input = Console.ReadLine(); //Можно ли как нибуть убрать ошибку CS8600 и её следствие CS8604, кромя способа с warning disable?
 
-            Console.WriteLine("Здравствуйте " + input + ".");
+            GreetUser(input);
 
             return 0;
         }
